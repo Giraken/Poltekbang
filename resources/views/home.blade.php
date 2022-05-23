dbf3eb3731ec0da17a690c35aa6b8883ddd2d9e1
Giraken <kagicrhis@gmail.com>
2022-05-23 15:04:41 +0700

    home(belum selesai)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,297 @@
 @extends('layouts.app')
 @section('beranda', 'active')
+@section('content')
+<style>
+    img:hover{
+        opacity: 0.5;
+    }
+</style>
+    <img src="img/gerbang3.jpeg" alt="gerbang" style="width:100%;opacity:1">
+    <div style="
+        position: absolute;
+        top: 30%;
+        color:#FFFFFF;
+    ">
+        <p style="
+            font-family: 'Montserrat';
+            font-style: normal;
+            font-weight: 700;
+            font-size: 55px;
+            line-height: 70px;
+            text-align: center;
+        ">
+            Menyelenggarakan program pendidikan vokasi, penelitian, dan pengabdian kepada masyarakat di bidang penerbangan.</p>
+    </div>
+    <div style="
+        position: absolute;
+        top: 65%;
+        left: 32%;
+        transform: translate(-10%, -10%);
+        color:#FFFFFF
+    ">
+        <p style="
+            font-family: 'Montserrat';
+            font-style: normal;
+            font-weight: 700;
+            font-size: 25px;
+            line-height: 35px;
+            text-align: center;
+        ">
+            Taruna dan Taruni Poltekbang Surabaya belajar dan hidup di kampus,<br>
+            sehingga menjadikan kampus sebagai rumah kedua mereka </p>
+    </div>
+
+    <div style="
+        position: absolute;
+        top: 75%;
+        left: 25%;
+        transform: translate(-10%, -10%);
+        color:#FFFFFF
+    ">
+        <p style="
+            font-family: 'Montserrat';
+            font-style: normal;
+            font-weight: 700;
+            font-size: 25px;
+            line-height: 29px;
+            text-align: center;
+        ">
+            Education and training supported by facilities and infrastructures with modern technology</p>
+    </div>
+
+    <div style="
+        top:115%;
+        left:40%;
+        position: absolute;
+
+    ">
+        <p style="font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 64px;
+        line-height: 96px;
+        text-align: center;
+
+        color: #474747;">
+        Look At Us</p>
+    </div>
+
+    <a href="">
+        <div style="
+            top:133%;
+            left:7%;
+            position: absolute;
+        ">
+            <img src="img/4.jpg" alt="about us" style="width: 75%;filter:brightness(70%)">
+            <div style="
+                top:50%;
+                left:25%;
+                position: absolute;
+            ">
+                <p style="
+                    font-family: 'Poppins';
+                    font-style: normal;
+                    font-weight: 700;
+                    font-size: 36px;
+                    line-height: 54px;
+                    text-align: center;
+                    color: #FFFFFF;
+                ">
+                    About Us</p>
+            </div>
+        </div>
+    </a>
+
+    <a href="">
+        <div style="
+            top:133%;
+            left:37%;
+            position: absolute;
+        ">
+            <img src="img/5.jpg" alt="about us" style="width: 125.5%;filter:brightness(70%)">
+            <div style="
+                top:50%;
+                left:40%;
+                position: absolute;
+            ">
+                <p style="
+                    font-family: 'Poppins';
+                    font-style: normal;
+                    font-weight: 700;
+                    font-size: 36px;
+                    line-height: 54px;
+                    text-align: center;
+                    color: #FFFFFF;
+                ">
+                    Message</p>
+            </div>
+        </div>
+    </a>
+
+    <a href="">
+        <div style="
+            top:133%;
+            left:65.5%;
+            position: absolute;
+        ">
+            <img class="hover" src="img/6.jpg" alt="about us" style="width: 136.7%;filter:brightness(70%)">
+            <div style="
+                top:50%;
+                left:45%;
+                position: absolute;
+            ">
+                <p style="
+                    font-family: 'Poppins';
+                    font-style: normal;
+                    font-weight: 700;
+                    font-size: 36px;
+                    line-height: 54px;
+                    text-align: center;
+                    color: #FFFFFF;
+                ">
+                    Contact</p>
+            </div>
+        </div>
+    </a>
+
+    <div class="Poltekbang Surabaya" style="
+        top:190%;
+        left:2%;
+        position: absolute;
+
+    ">
+        <p style="font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 64px;
+        line-height: 96px;
+        text-align: center;
+
+        color: #474747;">
+        Poltekbang Surabaya</p>
+    </div>
+
+    <div style="
+        top:200%;
+        position: absolute;
+    ">
+        <img src="img/poltekbang.jpg" alt="about us" style="opacity: 1.0;filter:brightness(50%);width:185.9%">
+    </div>
+
+    <div style="
+        top:210%;
+        left:72%;
+        position: absolute;
+
+    ">
+        <p style="
+        font-family: 'Montserrat';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 42px;
+        line-height: 51px;
+        text-align: center;
+
+        color: #FFFFFF;">
+        Lembaga Pendidikan</p>
+    </div>
+
+    <div style="
+        top:220%;
+        left:15%;
+        right:3%;
+        position: absolute;
+
+    ">
+        <p style="
+        font-family: 'Montserrat';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 24px;
+        line-height: 29px;
+        text-align: right;
+
+        color: #FFFFFF;">
+            Politeknik Penerbangan (Poltekbang) Surabaya (dh ATKP Surabaya) merupakan Lembaga Pendidikan Vokasi bidang aviasi yang Kredibel,
+            baik dari lembaganya sendiri maupun dari dosen & pengajarnya. Terbukti dengan lulusannya yg handal dalam memberikan pelayanan pengendalian
+            lalu lintas udara  di bandara yg sibuk di Indonesia dan sebagian dari alumninya sekarang sudah menduduki lower manager dalam usia yg relatif
+            masih muda. <br><br>
+            M. Khatim <br>
+            - Direktur Operasi Airnav Indonesia</p>
+    </div>
+
+    <div style="
+        top:270%;
+        left:2%;
+        position: absolute;
+
+    ">
+        <p style="
+        font-family: 'Montserrat';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 42px;
+        line-height: 51px;
+        text-align: center;
+
+        color: #FFFFFF;">
+        Professional</p>
+    </div>
+
+    <div style="
+        top:280%;
+        left:2%;
+        rigth:5%;
+        position: absolute;
+
+    ">
+        <p style="
+        font-family: 'Montserrat';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 24px;
+        line-height: 29px;
+        text-align: left;
+
+        color: #FFFFFF;">
+            Alumni ATKP/Poltekbang SBY terkenal memiliki sikap yang Baik, Respect dan peduli kepada orang lain <br> serta memiliki
+            pengetahuan yang paripurna di bidangnya, sehingga dapat diterima di dunia Aviasi Indonesia. <br> <br>
+            Alvin Surya Widiantara (Enroute 2007) <br>
+            - Airnav Indonesia, Alumni RKP 2A Tahun 2009	</p>
+    </div>
+
+    <div class="Contact" style="
+        top:325%;
+        left:83%;
+        position: absolute;
+
+    ">
+        <p style="font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 64px;
+        line-height: 96px;
+        text-align: center;
+
+        color: #474747;">
+        Contact </p>
+    </div>
+
+    <div style="
+        top:335%;
+        position: absolute;
+        background-color:#002E3B;
+        color:#99abb8;
+
+    ">
+        <p>OUR ADDRESS #ffffff;</p>
+        font-family: Raleway;
+        <p>Poltekbang Surabaya
+            Jalan Jemur Andayani I No 73 Surabaya 60236 <br>
+            Phone:62 31 8410871 <br>
+            Fax: 62 31 8490005 <br>
+            Email:mail@poltekbangsby.ac.id
+        </p>
+    </div>
+
+@endsection
